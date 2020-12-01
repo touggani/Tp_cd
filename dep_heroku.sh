49896771428861602c13e4b8e88af47de3fe8155
Youssef Touggani <yousseftouggani@youss-2.local>
2020-12-01 17:18:27 +0100

    test

--- dep_heroku.sh
+++ dep_heroku.sh
@@ -1,4 +1,4 @@
-HTTPCODE=`curl -s -o /dev/null -w "%{http_code}" https://$HEROKU_APP_NAME.herokuapp.com/`
+HTTPCODE=`curl -s -o /dev/null -w "%{http_code}" https://fierce-atoll-83490.herokuapp.com/`
 if [ "$HTTPCODE" -ne 200 ];then
     echo "heroku app not responding, failing deploy"
     exit 1
